2fc27c998a68140b719fcfbf6db5f788e0b6baaa
NickW16 <171855220+NickW16@users.noreply.github.com>
2026-09-18 16:21:47 -0300

    Implement delete functionality

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "randomArrayCreator.h"
 #include "arraySorter.h"
 #include "node.h"
@@ -44,6 +46,56 @@ struct Node* insert(struct Node* root, int value) {
 	return root;
 }
 
+struct Node* getSuccessor(struct Node* curr) {
+	curr = curr->right;
+	while (curr != NULL && curr->left != NULL) {
+		curr = curr->left;
+	}
+	return curr;
+}
+
+struct Node* deleteItem(struct Node* root, int value) {
+	if (root == NULL) {
+		printf("Attempted to delete node %d but it was not found!\n", value);
+		return root;
+	}
+
+	// search for key
+	if (root->data > value) {
+		root->left = deleteItem(root->left, value);
+	} else if (root->data < value) {
+		root->right = deleteItem(root->right, value);
+	} else {
+
+		//cases when root has no children or only one child
+		if (root->left == NULL && root->right == NULL) {
+			printf("%d deleted successfully\n", value);
+			free(root);
+			return NULL;
+		}
+
+		if (root->left == NULL) {
+			struct Node* temp = root->right;
+			printf("%d deleted successfully\n", value);
+			free(root);
+			return temp; // only right child
+		}
+		if (root->right == NULL) {
+			struct Node* temp = root->left;
+			printf("%d deleted successfully\n", value);
+			free(root);
+			return temp; // only left child
+		}
+
+		// find successor:
+		struct Node* successor = getSuccessor(root);
+		root->data = successor->data;
+		root->right = deleteItem(root->right, successor->data);
+	}
+
+	return root;
+}
+
 // print a tree
 void printSpaces(int count) {
 	for (int i = 0; i < count; i++) {
@@ -71,6 +123,14 @@ void printTree(struct Node* root) {
 	printf("\nBalanced BST:\n");
 	printTreeHelper(root, 0, 5);
 	printf("\n");
+}
+
+void freeTree(struct Node* root) {
+	if (root != NULL) {
+		freeTree(root->left);
+		freeTree(root->right);
+		free(root);
+	}
 }
 
 int main() {
@@ -111,7 +171,13 @@ int main() {
 	printf("inserting number: 50\n");
 	root = insert(root, 50);
 
+	printf("Deleting numbers 80, 50 and 37 (if exists)\n");
+	root = deleteItem(root, 80);
+	root = deleteItem(root, 50);
+	root = deleteItem(root, 37);
+
 	printTree(root);
 
+	freeTree(root);
 	return 0;
 }
